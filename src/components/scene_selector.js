a675fc5904193bb55ed352aa85e2a2b1092d9790
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2019-02-18 15:46:48 -0500

    even more deletion logic. todo: test

--- src/components/scene_selector.js
+++ src/components/scene_selector.js
@@ -7,19 +7,21 @@ Vue.component('scene-selector', {
     <transition-group name="fade">
       <div v-bind:key="scenes[sceneNumber].id" 
            v-for="(sceneNumber, sceneIndex) in scenesIndex"
-           v-on:click="switch_to(sceneIndex)"
            v-bind:class="'scene' + ' ' + (selected == sceneIndex? 'selected-scene': '')">
         <img src="icons/feather/copy.svg" 
              title="copy scene"
              v-on:click="copyScene(sceneIndex)"
              width="20"
              class="copy-scene"/>
-         <img src="icons/feather/trash.svg" 
+        <img src="icons/feather/trash.svg" 
              title="remove scene"
              v-on:click="remove(sceneIndex)"
              v-if="scenesIndex.length > 1"
              width="20"
              class="remove-scene"/>
+        <img v-on:click="switch_to(sceneIndex)"
+             src=""
+             v-bind:class="'scene-preview scene-preview-' + scenes[sceneNumber].id"/>
       </div>
     </transition-group>
     <div class="adder-container">
@@ -88,13 +90,14 @@ Vue.component('scene-selector', {
 	  }
 	  
 	  if(sceneIndex == this.selected){
-		if(sceneIndex >= this.scenesIndex.length - 2){
+		if(sceneIndex >= this.scenesIndex.length - 1){
 		  this.switch_to(sceneIndex - 1);
 		} else {
-		  this.switch_to(sceneIndex + 1);
+		  this.switch_to(sceneIndex);
 		}
+	  } else if (this.selected > sceneIndex) {
+		this.switch_to(this.selected - 1);
 	  }
-
 	  
       // Decrement all elements after current index
       this.scenesIndex = this.scenesIndex.map((i) => { return i <= sceneIndex? i: i - 1; });
