29a85969391a0f8c28a1d3d44ec755dda8f2d64e
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2019-03-25 20:15:05 -0400

    Better version of gfont picker

--- src/plugins/gfontTextLayer/settings.js
+++ src/plugins/gfontTextLayer/settings.js
@@ -7,7 +7,7 @@
       human_name: "Google Font Text Layer",
       ui: {
         template: `
-<div>
+<div class="gfont-plugin">
   <h4>Text</h4>
   <label>Your text</label>
   <input v-model="text.text" type="text">
@@ -16,14 +16,15 @@
   <input v-model.number="text.offsetTop" type="number" size="4" step="25">
   <input v-model.number="text.offsetLeft" type="number" size="4" step="25">
   <input v-model="text.color" type="color">
-  <map name="gfonts">
-    <area v-for="info in fonts"
-          v-on:click="font = info.font"
-          shape="rect"
-          v-bind:coords="[info.x1, info.y1, info.x2, info.y2].join(' ')"
-          />
-  </map>
-  <img usemap="#gfonts" src="./plugins/gfontTextLayer/fonts.png">
+  <div class="gfont-scrollbox">
+    <div v-for="info in fonts">
+      <span class="raw-fontname">{{info.font}}</span>
+      <button class="gfont-button"
+              v-on:click="changeFont(info.font)">
+        <img v-bind:data-fontName="info.font" v-bind:alt="info.font"/>
+      </button><br>
+    </div>
+  </div>
   <h4>Background</h4>
   <label>Transparent background
     <input type="checkbox" v-model="transparentBackground">
@@ -50,6 +51,10 @@
         },
         props: ["player", "shaderProgram"],
         methods: {
+          changeFont(fontName){
+            this.font = fontName;
+            this.updateTexts();
+          },
           updateTexts(){
             let textCanvas = document.createElement("canvas");
             let ctx = textCanvas.getContext("2d");
@@ -88,6 +93,26 @@
               textCanvas.toDataURL(),
               function(){}
             );
+          },
+          updateFontPreviews(){
+            canvas = document.createElement("canvas");
+            ctx = canvas.getContext("2d");
+            canvas.width = 280;
+            canvas.height = 48;
+
+            for(font in this.fonts){
+              let fontInfo = this.fonts[font];
+              let image = this.$el.querySelectorAll("[data-fontName='" + fontInfo.font + "']")[0];
+              let img = this.img;
+
+              ctx.fillStyle = "#ffffff";
+              ctx.fillRect(0,0,canvas.width,canvas.height);
+
+              ctx.drawImage(img, fontInfo.x1, fontInfo.y1, 192, 48, 0.5*(280-192), 0, 192, 48);
+              canvas.toBlob(function(blob){
+                image.src = URL.createObjectURL(blob);
+              });
+            }
           }
         },
         watch: {
@@ -118,10 +143,18 @@
           let app = this;
           this.updateTexts();
 
+          let img = document.createElement("img");
+          img.onload = function(){};
+          img.src = "./plugins/gfontTextLayer/fonts.png";
+          this.img = img;
+
           fetch("./plugins/gfontTextLayer/fonts.json").then(function(resp){
             resp.json()
               .then(function(data){
                 app.fonts = data;
+                app.$nextTick(function(){
+                  app.updateFontPreviews();
+                });
               });
           });
         }
