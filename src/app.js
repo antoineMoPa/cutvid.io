86fe92e10551a61a4574867f09d27e7d6e54b64e
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2019-03-31 14:18:47 -0400

    Use settings

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 var app = new Vue({
   el: '#main_app',
   data: {
+	settings: null
   },
   template: `<div>
                  <player></player>
              </div>`,
+  mounted(){
+	fetch("settings.json").then((resp) => {
+	  resp.json().then((data) => {
+		window.settings = data;
+	  });
+	});
+  }
 })
