0ded2592620c94cd2c1f99ac8b1c58c3444a6534
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2019-04-01 09:19:53 -0400

    Start services in dev server

--- dev-server.sh
+++ dev-server.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+
 cd src
-python -m SimpleHTTPServer
+python -m SimpleHTTPServer &
+cd ..
+
+cd services
+
+cd converter
+./start.bash &
+cd ..
+
+cd downloadables
+./start.bash &
+cd ..
