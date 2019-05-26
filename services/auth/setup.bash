01713d7c8d0d43dacbee824e5e91e90328122ac9
Antoine <antoine.morin.paulhus@gmail.com>
2019-05-26 15:19:15 -0400

    setup++

--- services/auth/setup.bash
+++ services/auth/setup.bash
@@ -2,16 +2,15 @@
 
 # Run as root
 
-apt-get install ruby-dev
-apt-get install nodejs
-gem install bundler
+sudo apt-get install ruby-dev nodejs mysql-server mysql-client
+sudo gem install bundler
 
-echo "CREATE USER 'auth_development'@'localhost' IDENTIFIED BY 'auth_dev_password';" |  mysql;
-echo "GRANT ALL PRIVILEGES ON auth_development.* TO 'auth_development'@'localhost';" |  mysql;
-echo "GRANT ALL PRIVILEGES ON auth_test.* TO 'auth_development'@'localhost';" |  mysql;
+bundle install --path vendor/bundle
+
+echo "CREATE USER 'auth_development'@'localhost' IDENTIFIED BY 'auth_dev_password';" |  sudo mysql;
+echo "GRANT ALL PRIVILEGES ON auth_development.* TO 'auth_development'@'localhost';" |  sudo mysql;
+echo "GRANT ALL PRIVILEGES ON auth_test.* TO 'auth_development'@'localhost';" |  sudo mysql;
 
 bundle exec rake db:create
 bundle exec rake db:migrate
-bundle exec rails server
-
 
