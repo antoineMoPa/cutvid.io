8d2ba3fb1d987c7c5db918ce774fd7a83f4407f1
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2019-11-24 19:29:35 -0500

    Set root path

--- services/auth/config/application.rb
+++ services/auth/config/application.rb
@@ -11,6 +11,7 @@ module Auth
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.relative_url_root = "/auth"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
