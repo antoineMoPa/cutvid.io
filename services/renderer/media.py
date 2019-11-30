6f43ec4e0494bbe512a25d39b80857c2289bdf47
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2019-11-30 18:05:37 -0500

    Remove untested media routes

--- services/renderer/media.py
+++ services/renderer/media.py
@@ -20,68 +20,6 @@ from utils import *
 
 media = Blueprint('media', __name__)
 
-@media.route("/media/<project_id>", methods=['GET', 'OPTIONS'])
-def list_media():
-    """
-    List a user's project media
-    """
-
-    if request.method == 'OPTIONS':
-        return ""
-
-    token = read_token(request.headers['Authorization'].replace("Bearer ", ""))
-
-    if token is None:
-        return "error 1 bad token"
-
-    project_id = int(project_id)
-    user_id = int(token['user_id'])
-
-    user_folder = USERS_FOLDER + "user-" + str(user_id) + "/"
-    project_folder = user_folder + "project-" + str(project_id) + "/"
-    project_file_path = project_folder + "lattefx_file.lattefx"
-
-    if not os.path.exists(project_file_path):
-        return "error 8 no project file found"
-
-    project_media_files = glob.glob(project_folder + "/media/*")
-
-    return json.dumps(project_media_files)
-
-@media.route("/upload_media/<project_id>", methods=['POST', 'OPTIONS'])
-def upload_media(project_id):
-    """
-    returns media id
-    """
-
-    if request.method == 'OPTIONS':
-        return ""
-
-    token = read_token(request.headers['Authorization'].replace("Bearer ", ""))
-
-    if token is None:
-        return "error 1 bad token"
-
-    project_id = int(project_id)
-    user_id = int(token['user_id'])
-
-    user_folder = USERS_FOLDER + "user-" + str(user_id) + "/"
-    os.makedirs(user_folder, exist_ok=True)
-
-    project_folder = user_folder + "project-" + str(project_id) + "/"
-    os.makedirs(project_folder, exist_ok=True)
-
-    project_media_folder = project_folder + "media/"
-    os.makedirs(project_media_folder, exist_ok=True)
-
-    media_name = id_generator()
-    media_path = project_media_folder + media_name
-    media_file = request.files['media']
-    media_file.save(media_path)
-
-    return media_name
-
-
 @media.route("/media/<project_id>/<media_id>", methods=['GET', 'OPTIONS'])
 def get_media(project_id, media_id):
     """
