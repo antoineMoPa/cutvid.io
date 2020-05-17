1cd7cdbaaeb1fe5de04cac336f985430c0090bfe
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2020-05-17 12:15:15 -0400

    Fix migration for prod

--- services/auth/db/migrate/20200516191356_remove_render_credit_renew_date.rb
+++ services/auth/db/migrate/20200516191356_remove_render_credit_renew_date.rb
@@ -1,5 +1,6 @@
 class RemoveRenderCreditRenewDate < ActiveRecord::Migration[5.2]
   def change
-    remove_column :users, :render_credit_renew_date
+    # That column was ever only in my dev env?
+    # remove_column :users, :render_credit_renew_date
   end
 end
