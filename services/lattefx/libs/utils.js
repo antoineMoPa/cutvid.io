30b06e1961a77433544039bdd7d0ad163774a5e9
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2020-05-18 12:29:42 -0400

    Accurate progress bar for video previews

--- services/lattefx/libs/utils.js
+++ services/lattefx/libs/utils.js
@@ -501,6 +501,58 @@ var utils = {};
 
   utils.small_videos_cache = {};
 
+  utils.get_video_total_frames= async function(video_file){
+    await Promise.all([
+      utils.load_script("libs/ffmpeg/ffmpeg.min.js")
+    ]);
+    let worker;
+
+    const { createWorker } = FFmpeg;
+
+    let fps_regex = /\s*([0-9]*) fps/;
+    let duration_regex = /Duration: ([0-9]*):([0-9]*):([0-9\.]*)/;
+    let fps = null;
+    let duration_in_seconds = null;
+
+    try{
+      worker = createWorker({
+        corePath: "libs/ffmpeg/ffmpeg-core.js",
+        logger: function(m){
+          if(fps_regex.test(m.message)){
+            fps = fps_regex.exec(m.message)[1];
+          }
+          if(duration_regex.test(m.message)){
+            let match = duration_regex.exec(m.message);
+
+            duration_in_seconds = match[3];
+            duration_in_seconds += match[2] * 60;
+            duration_in_seconds += match[1] * 60 * 60;
+          }
+        }
+      });
+    } catch (e) {
+      // Don't care
+    }
+
+    await worker.load();
+
+    await worker.write(
+      "video_to_convert.video",
+      video_file
+    );
+
+    // Get duration for progress bar
+    try{
+      await worker.run("-i video_to_convert.video");
+    } catch (e) {
+      // Don't care too much
+    }
+
+    let total_frames = fps * duration_in_seconds;
+
+    return total_frames;
+  };
+
   utils.make_small_video = async function(video_file, file_name){
     let cache_name = file_name;
 
@@ -511,19 +563,20 @@ var utils = {};
       }
     }
 
-    window.API.call("ui.set_progress", 0.05, "Loading ffmpeg.");
+    window.API.call("ui.set_progress", 0.01, "Loading ffmpeg.");
 
     await Promise.all([
       utils.load_script("libs/ffmpeg/ffmpeg.min.js")
     ]);
 
-    window.API.call("ui.set_progress", 0.3, "Building video preview.");
+    window.API.call("ui.set_progress", 0.02, "Getting video duration.");
+    let total_frames = await utils.get_video_total_frames(video_file);
 
     let worker;
 
     const { createWorker } = FFmpeg;
 
-    let last_progress = 0.0;
+    let frame_regex = /frame=\s*([0-9]*)/;
 
     worker = createWorker({
       corePath: "libs/ffmpeg/ffmpeg-core.js",
@@ -535,17 +588,18 @@ var utils = {};
             "We encountered an error while converting your file."
           );
           window.API.call("ui.clear_progress");
-          console.log(m);
         }
-        if(m.message.indexOf("frame=") != -1){
-          // Advance progress a bit
-          last_progress += 0.2;
-          let entertain_progress = 0.3 + (0.7-0.7/(last_progress + 1.0));
-          window.API.call("ui.set_progress", entertain_progress, "Building video preview.");
+        if(frame_regex.test(m.message)){
+          let match = frame_regex.exec(m.message);
+          let frame = match[1];
+          let progress = frame / total_frames;
+          window.API.call("ui.set_progress", progress,
+                          `Building video preview : frame ${frame} of ${total_frames}.`);
         }
       }
     });
 
+    window.API.call("ui.set_progress", 0.15, "Generating preview: You can keep working.");
     await worker.load();
 
     await worker.write(
@@ -553,7 +607,10 @@ var utils = {};
       video_file
     );
 
-    await worker.run("-i video_to_convert.video -vf scale=48x48 -sws_flags neighbor -pix_fmt yuv420p output.mp4");
+    // Make video
+    await worker.run("-i video_to_convert.video -vf scale=48x48 "+
+                     "-sws_flags neighbor -pix_fmt yuv420p " +
+                     "output.mp4");
 
     let result = await worker.read("output.mp4");
 
@@ -580,7 +637,7 @@ var utils = {};
 
           Makes a small version of a video (for quick seek previews)
 
-          video is around 48:48px at 15 fps (fps may vary greatly)
+          video is around 48:48px
           `,
     argsdoc: ["Original video file", "Unique name for cache"],
     no_ui: true,
@@ -616,7 +673,6 @@ var utils = {};
             "We encountered an error while converting your file."
           );
           window.API.call("ui.clear_progress");
-          console.log(m);
         }
         if(m.message.indexOf("frame=") != -1){
           // Advance progress a bit
