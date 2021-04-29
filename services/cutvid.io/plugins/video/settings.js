0a05b167f2631fe9d0df9dc7b5e842fc292bcea0
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2021-04-28 22:08:49 -0400

    Make video opacity 0 until a video is added. #15

--- services/cutvid.io/plugins/video/settings.js
+++ services/cutvid.io/plugins/video/settings.js
@@ -97,7 +97,7 @@
               opacity: {
                 type: "f",
                 len: 0,
-                value: 1,
+                value: 0,
               },
               isLoaded: {
                 type: "f",
@@ -284,6 +284,7 @@
 
             this.file_store.files[name] = file;
             this.uniforms.isLoaded.value = 1.0;
+            this.uniforms.opacity.value = 1.0;
             this.file_name = name;
 
             this.shaderProgram.set_texture('video', name, this.video_ready);
