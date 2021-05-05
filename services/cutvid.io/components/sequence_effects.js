antoineMoPa/cutvid.io

commit 0a05b167f2631fe9d0df9dc7b5e842fc292bcea0

Make video opacity 0 until a video is added. #15

services/cutvid.io/plugins/video/settings.js
@@ -97,7 +97,7 @@
               opacity: {
                 type: "f",
                 len: 0,
-                value: 1,
+                value: 0,
               },
               isLoaded: {
                 type: "f",
@@ -284,6 +284,7 @@
 
             this.file_store.files[name] = file;
             this.uniforms.isLoaded.value = 1.0;
+            this.uniforms.opacity.value = 1.0;
             this.file_name = name;
 
             this.shaderProgram.set_texture('video', name, this.video_ready);

commit 1d410dd28f946588988bf50218d4516f31fd98f7

Fix drag and drop that was broken by 0a05b167f2631fe9d0df9dc7b5e842fc292bcea0

services/cutvid.io/components/sequence_effects.js
@@ -193,7 +193,6 @@ void main(){
           plugin.shaderProgram = _shaderProgram;
           plugin.effect.shaderProgram = _shaderProgram;
 
-
           // Insert effect in array
           app.effect = settings;
 

services/cutvid.io/components/sequencer.js
@@ -1172,6 +1172,8 @@ Vue.component('sequencer', {
         e.preventDefault();
         let [x,y,time,layer,seq,duration,scale] = this.mouse_event_info(e);
         this.time.time = time;
+
+        return false;
       }.bind(this), false);
 
       window.addEventListener("drop", async function(e){
@@ -1191,7 +1193,7 @@ Vue.component('sequencer', {
         } else if(type.indexOf("audio") != -1){
           sequence_type_to_add = "audio";
         } else {
-          return;
+          return false;
         }
 
         let file_store = window.API.call("shader_player.get_file_store");
@@ -1218,6 +1220,7 @@ Vue.component('sequencer', {
         }], false);
         added_sequence = null;
 
+        return false;
       }.bind(this), false);
     }
   },
@@ -1259,19 +1262,7 @@ Vue.component('sequencer', {
     allSequencesContainer.appendChild(allSequences);
 
     this.bindShortcuts();
-
-    if (false /* Video upload has a problem */) {
-      this.bind_drag_and_drop();
-    }
-
-    window.addEventListener("dragover", function(e){
-      e.preventDefault();
-    });
-
-    window.addEventListener("drop", function(e){
-      e.preventDefault();
-      console.log("drop!");
-    });
+    this.bind_drag_and_drop();
 
     this.reposition_sequences();
     this.resize();

services/cutvid.io/plugins/video/settings.js
@@ -62,6 +62,7 @@
             backgroundColor: "#000000",
             trimBefore: 0,
             durationInitialized: false,
+            videoHasLoadedOnce: false,
             duration_sent: false,
             preview_built: false,
             player: null,
@@ -283,8 +284,6 @@
             }
 
             this.file_store.files[name] = file;
-            this.uniforms.isLoaded.value = 1.0;
-            this.uniforms.opacity.value = 1.0;
             this.file_name = name;
 
             this.shaderProgram.set_texture('video', name, this.video_ready);
@@ -299,8 +298,14 @@
             this.trimBefore += diff;
           },
           async video_ready(video){
-            this.uniforms.videoWidth.value = video.videoWidth;
-            this.uniforms.videoHeight.value = video.videoHeight;
+            if (!this.videoHasLoadedOnce) {
+              this.uniforms.isLoaded.value = 1.0;
+              this.uniforms.opacity.value = 1.0;
+              this.uniforms.videoWidth.value = video.videoWidth;
+              this.uniforms.videoHeight.value = video.videoHeight;
+            }
+
+            this.videoHasLoadedOnce = true;
 
             if(!this.duration_sent){
               this.onDuration(video.duration);
@@ -328,6 +333,7 @@
           },
           async file_name(file_name){
             this.shaderProgram.set_texture('video', this.file_name, this.video_ready);
+            this.videoHasLoadedOnce = false;
 
             let api = window.API;
 